131f7650c4cd7eefa0793f0b61530d351141a194
KhangSPR <129411252+KhangSPR@users.noreply.github.com>
2025-04-22 15:50:52 +0700

    video 7 create Pipe Holder

---
+++ Assets/_Script/PipeCtrl/PipeHolder.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeHolder : MonoBehaviour
+{
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        _PipeMovent();
+    }
+    private void _PipeMovent()
+    {
+        Vector3 temp = transform.position;
+        temp.x -= speed* Time.deltaTime;
+        transform.position = temp;
+    }
+
+}
