0a3cf0293b9aa36d41cdc5efce5aaf274e25204b
KhangSPR <129411252+KhangSPR@users.noreply.github.com>
2025-04-22 15:02:07 +0700

    video 5 wiriting script plappy bird part 2

--- Assets/_Script/BirdCtrl/BirdCtrl.cs
+++ Assets/_Script/BirdCtrl/BirdCtrl.cs
@@ -9,6 +9,12 @@ public class BirdCtrl : MonoBehaviour
     private Rigidbody2D rdBody2d;
     private Animator animator;
 
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip flyClip, pingClip, dieClip;
+
     private void Awake()
     {
         rdBody2d = GetComponent<Rigidbody2D>();
@@ -16,12 +22,29 @@ public class BirdCtrl : MonoBehaviour
     }
     void Update()
     {
-        BirdFly();
+        _BirdFly();
     }
-    void BirdFly()
+    void _BirdFly()
     {
         if(Input.GetMouseButtonUp(0)) {
-            rdBody2d.velocity = new Vector2(rdBody2d.velocity.x, boundForce);   
+            rdBody2d.velocity = new Vector2(rdBody2d.velocity.x, boundForce);
+            audioSource.PlayOneShot(pingClip);
+        }
+
+        if(rdBody2d.velocity.y >0)
+        {
+            float angle = 0;
+            angle = Mathf.Lerp(0, 90, rdBody2d.velocity.y / 7);
+            transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        }else if(rdBody2d.velocity.y == 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        else
+        {
+            float angle = 0;
+            angle = Mathf.Lerp(0, -90, -rdBody2d.velocity.y / 7);
+            transform.rotation = Quaternion.Euler(0f, 0f, angle);
         }
     }
 }
